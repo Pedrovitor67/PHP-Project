4243991e274d900922016e44fe30e88ecc46087a
Pedrovitor67 <vitorpedrovitor926@gmail.com>
2024-11-18 10:51:53 -0300

    Revisão do código. Comentários adicionados para melhor separar as seções de produtos no html Comentários adicionados ao slider.

--- src/slider.js
+++ src/slider.js
@@ -1,24 +1,25 @@
 
 
 //Primeiro Slider
-const slider = document.querySelector(".products__container"); //Container de produtos
-const leftButton = document.querySelector(".products__arrowLeft"); //Seta esquerda
-const rightButton = document.querySelector(".products__arrowRigth"); //Seta direita
+const slider = document.querySelector(".products__container"); 
+const leftButton = document.querySelector(".products__arrowLeft"); 
+const rightButton = document.querySelector(".products__arrowRigth"); 
 
 //Segundo Slider
-const slider2 = document.querySelector(".products__container2"); //Container de produtos
-const leftButton2 = document.querySelector(".products__arrowLeft2"); //Seta esquerda
-const rightButton2 = document.querySelector(".products__arrowRigth2"); //Seta direita
+const slider2 = document.querySelector(".products__container2");
+const leftButton2 = document.querySelector(".products__arrowLeft2");
+const rightButton2 = document.querySelector(".products__arrowRigth2"); 
 
 
 function getScrollDistance() {
-  const screenWidth = window.innerWidth;
-
-  // Ajusta a distância  do scroll com base em tamanhos de tela
+  const screenWidth = window.innerWidth; 
+  
   if (screenWidth > 1024) {
     return 1600; // Para telas grandes
+
   } else if (screenWidth > 768) {
     return 500; // Para tablets
+
   } else {
     return 370; // Para celulares
   }
@@ -42,13 +43,14 @@ function setupSlider(slider, leftButton, rightButton) {
     });
   }
 
-  // Inicializa os sliders
+  // Inicializa os sliders.Esperando que todo o conteúdo da página,DOM, seja carregado.
 document.addEventListener("DOMContentLoaded", () => {
     setupSlider(slider, leftButton, rightButton);
     setupSlider(slider2, leftButton2, rightButton2);
   });
 
 
+  //Feedback da quantidade de Scroll no console.
 window.addEventListener("resize", () => {
   console.log(`Nova distância de scroll: ${getScrollDistance()}px`);
 });
