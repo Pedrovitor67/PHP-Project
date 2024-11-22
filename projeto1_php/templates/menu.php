56c84a89ddfb8daa108641dc4a9c09682b4c6e5f
Júlia Marques <jm9229238@gmail.com>
2024-11-22 16:09:57 -0300

    ajustes

--- projeto1_php/templates/menu.php
+++ projeto1_php/templates/menu.php
@@ -11,5 +11,5 @@
     <a href="?pg=conteudo">Principal</a> |
     <a href="?pg=quemsomos">Quem somos</a> |
     <a href="?pg=faleconosco">Contato</a>
-    <a href="?pg=cadastrar"> Cadrastar </a>
+    <a href="?pg=cadastra.php"> Cadastar </a>
 </nav>
